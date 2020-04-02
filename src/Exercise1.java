7962174ba857454b6ad04da78927abe9ffba2143
IvHaaften <Inge@van-haaften.nl>
2020-04-02 12:13:24 +0200

    Testing whether the github link is properly working

--- src/Exercise1.java
+++ src/Exercise1.java
@@ -3,5 +3,6 @@ public class Exercise1 {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("test test");
+        System.out.println("Is the VCS working now?");
     }
 }

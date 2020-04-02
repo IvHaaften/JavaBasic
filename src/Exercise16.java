4fc615a78ee8d817f7bfe29d402bc46d2a08b3ed
IvHaaften <Inge@van-haaften.nl>
2020-04-02 14:56:20 +0200

    Finished exercise 16

--- src/Exercise16.java
+++ src/Exercise16.java
@@ -3,5 +3,14 @@ public class Exercise16 {
         //goal:
         /*Write a program that displays the first 10 numbers in the following sequence:
             5, 9, 13, 17, 21, .... */
+
+        //setting the first number of the sequence
+        int number = 5;
+        System.out.print("The sequence is : " + 5);
+
+        for(int i=0; i<9; i++){
+            number = number + 4;
+            System.out.print(", " + number);
+        }
     }
 }

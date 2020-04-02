84fbba670d4250e151c48adf699e255be8546c46
IvHaaften <Inge@van-haaften.nl>
2020-04-02 13:52:35 +0200

    Finished exercise 5: if statements

---
+++ src/Exercise5.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Exercise5 {
+    public static void main(String[] args) {
+        //goal:
+        /*Express each of the following statements using an if block:
+        a.If x divided by y is 5, then set x to 100.
+        b.If x times y is 5, then set x to 1.
+        c.If x is less than y, then double the value of x.
+        d.If x is greater than y, then increment the value of x by 1.*/
+
+        //initialising scanner
+        Scanner scanner = new Scanner(System.in);
+        //asking for the desired input
+        System.out.print("Please enter an integer to define x : ");
+        int x = scanner.nextInt();
+        System.out.print("Please enter an integer to define y : ");
+        int y = scanner.nextInt();
+
+        if((x/y)==5){
+            x=100;
+        }
+        if(x*y==5){
+            x=1;
+        }
+        if(x<y){
+            x=x*2;
+        }
+        if(x>y){
+            x++;
+        }
+        System.out.println("x is currently " + x);
+
+        //the exercise did not specify if only one case should apply or if they should be checked in turn
+    }
+}
